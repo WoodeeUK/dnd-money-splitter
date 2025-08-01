369ef421a73e6eec7ec236c1e5fdcfc54fa75fa7
WoodeeUK <its.woodee@gmail.com>
2025-08-01 17:30:53 +0100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'dnd-money-splitter-v1.1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+  // Add './styles.css', './script.js' if you separate those later
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
